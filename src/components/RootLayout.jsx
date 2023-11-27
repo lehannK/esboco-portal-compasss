232a637b8dc85a3a45d9931b47485162f3843663
leandro martins <leandro_hollies@hotmail.com>
2023-11-26 23:28:38 -0300

    responsivity-320px-ok

--- src/components/RootLayout.jsx
+++ src/components/RootLayout.jsx
@@ -50,7 +50,7 @@ export default () => {
               // target="_blank"
               href="/"
             >
-              <div className="text-xl">PORTAL DE SERVIÇOS </div>
+              <div className="text-xl">Portal de Serviços </div>
             </a>
             <button
               className="navbar-toggler"
@@ -112,110 +112,36 @@ export default () => {
         </nav>
       </div>
 
-      {/* <Outlet />
-      <footer>footer</footer> */}
+      {/* <Outlet />*/}
 
       <div className="fixed-bottom ">
-        <header className="topbar ">
-          {/* <div className="container">
-            <div className="row">
-              <div className="col-sm-12 ">
-                <ul className="social-network">
-                  <li>
-                    <a
-                      className="waves-effect waves-dark"
-                      target="_blank"
-                      href="https://br.linkedin.com/company/compasss-tecnologia"
-                    >
-                      <i className="bi bi-linkedin "></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      className="waves-effect waves-dark"
-                      target="_blank"
-                      href="https://www.instagram.com/compassstech/"
-                    >
-                      <i className="bi bi-instagram "></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      className="waves-effect waves-dark"
-                      target="_blank"
-                      href="https://www.facebook.com/compassstecnologia/?locale=pt_BR"
-                    >
-                      <i className="bi bi-facebook "></i>
-                    </a>
-                  </li>
-                </ul>
+        <footer className="footer  ">
+          <img
+            src="../assets/Ativo 3.png"
+            alt="logo-compasss"
+            className="logo-compasss"
+          ></img>
+          <div>
+            <div className="d-flex footer-info">
+              <i className="bi bi-geo-alt pl-1"></i>
+              <div className="text-light  pl-1 footer-text  ">
+                R. Leôncio de Carvalho, 306 - Cj 22
               </div>
-            </div> */}
-          {/* </div> */}
-        </header>
-        <nav className="footer  ">
-          <div className="container">
-            {/* <a
-              className="navbar-brand"
-              rel="nofollow"
-              // target="_blank"
-              href="/"
-            >
-              <div className="text-xl">PORTAL DE SERVIÇOS </div>
-            </a> */}
-            <img
-              src="../assets/Ativo 3.png"
-              alt="logo-compasss"
-              className="logo-compasss"
-            ></img>
-            ;"
-            {/* <div className="collapse navbar-collapse" id="navbarResponsive">
-              <ul className="navbar-nav ml-auto">
-                <li className="nav-item active">
-                  <a className="nav-link" href="/">
-                    Página Inicial
-                    <span className="sr-only">(current)</span>
-                  </a>
-                </li>
-
-                <li className="nav-item dropdown">
-                  <a
-                    className="nav-link dropdown-toggle"
-                    href="#"
-                    id="navbarDropdown"
-                    role="button"
-                    data-toggle="dropdown"
-                    aria-haspopup="true"
-                    aria-expanded="false"
-                  >
-                    Contato
-                  </a>
-                  <div
-                    className="dropdown-menu"
-                    aria-labelledby="navbarDropdown"
-                  >
-                    <a className="dropdown-item" href="#">
-                      Opção 1
-                    </a>
-                    <a className="dropdown-item" href="#">
-                      Opção 2
-                    </a>
-                    <div className="dropdown-divider"></div>
-                    <a className="dropdown-item" href="#">
-                      Outra opção
-                    </a>
-                  </div>
-                </li> */}
-            {/* 
-                <li className="nav-item">
-                  <a className="nav-link" href="#">
-                    Login
-                  </a>
-                </li>
-              </ul>
-            </div> */}
+            </div>
+            <div className="d-flex footer-info">
+              <i className="bi bi-envelope pl-1"></i>
+              <div className="text-light pl-1 footer-text  ">
+                comercialsp@compasss.com.br
+              </div>
+            </div>
+            <div className="d-flex footer-info">
+              <i className="bi bi-telephone pl-1"></i>
+              <div className="text-light pl-1 footer-text  ">
+                +55 (11) xxxxx-xxxx
+              </div>
+            </div>
           </div>
-        </nav>
+        </footer>
       </div>
     </>
   );
